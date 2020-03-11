ac3fd3fdfa1c6fbdfcde8d58f202e4c934d6fb18
supersuperMax <59124796+supersuperMax@users.noreply.github.com>
2020-03-11 23:10:42 +0300

    Update sever.js

--- Websyte/sever.js
+++ Websyte/sever.js
@@ -2,12 +2,6 @@ const express = require('express');
 const fs = require('fs');
 const app = express();
 app.get('/', function (req, res) { 
-  
-fs.readFile('map.html', (err, data) => {
-     if(err) throw err;
-     res.set('Content-Type', 'text/html; charset=utf-8');
-     res.set('Access-Control-Allow-Origin', '*');
-     res.send(data);
-});
+    res.sendFile('map.html');
 } );
 app.listen(3000);

35fcd7d588bf10a36303984fab268942959a1c2c
supersuperMax <59124796+supersuperMax@users.noreply.github.com>
2020-03-11 22:20:16 +0300

    Update sever.js

--- Websyte/sever.js
+++ Websyte/sever.js
@@ -1,4 +1,11 @@
 const express = require('express');
+const fs = require('fs');
 const app = express();
-app.get('/', function (req, res) { res.send('<h1>Hello World!</h1>'); } );
+app.get('/', function (req, res) { 
+  
+fs.readFile('sensor.json', (err, data) => {
+     if(err) throw err;
+     res.send(data);
+});
+} );
 app.listen(3000);

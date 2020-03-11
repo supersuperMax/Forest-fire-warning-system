b7f745558ffbd6aa2337447420633611397fc5eb
supersuperMax <59124796+supersuperMax@users.noreply.github.com>
2020-03-11 23:13:13 +0300

    Update sever.js

--- Websyte/sever.js
+++ Websyte/sever.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const fs = require('fs');
 const app = express();
+var path = require("path");
+
 app.get('/', function (req, res) { 
-    res.sendFile('map.html');
+    res.sendFile(path.join(__dirname + 'map.html'));
 } );
 app.listen(3000);
